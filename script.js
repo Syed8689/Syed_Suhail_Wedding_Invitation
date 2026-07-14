a08b58238ac5971fa21b96ea8af017b251f05fb0
Syed8689 <syedsaqibsaq@gmail.com>
2026-07-14 22:43:12 +0530

    Update script.js
    update9

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
-alert("SCRIPT LOADED");
+const openButton = document.getElementById("openInvitation");
 
-const btn = document.getElementById("openInvitation");
+const envelopeScreen = document.getElementById("envelope-screen");
 
-if(btn){
+const invitationContent = document.getElementById("invitation-content");
 
-    btn.onclick = function(){
+if (openButton) {
+  openButton.addEventListener("click", function () {
 
-        alert("BUTTON CLICKED");
+    envelopeScreen.style.opacity = "0";
 
-    };
+    setTimeout(() => {
 
+      envelopeScreen.style.display = "none";
+
+      invitationContent.style.display = "block";
+
+      invitationContent.style.opacity = "0";
+
+      setTimeout(() => {
+        invitationContent.style.opacity = "1";
+      }, 50);
+
+    }, 600);
+
+  });
 }
